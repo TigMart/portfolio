310cd7dc2ceb0ba5b642d7f368b57fd91735871e
Tigran <77511200+TigMart@users.noreply.github.com>
2024-04-09 13:49:28 +0400

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
         <title>{PAGETITLES.HOME}</title>
       </Helmet>
       <Header />
-      <h1 style={{ textAlign: 'center', color: 'white' }}>Coming Soon...</h1>
+      <h1 style={{ textAlign: 'center', color: 'white' }}>Coming Soon.....</h1>
     </>
   );
 }
